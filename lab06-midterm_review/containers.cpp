992937a43f0938a568928a829a47f7a449569707
tgroeche <tgroeche@umich.edu>
2018-03-05 19:58:21 -0500

    lab 6

--- lab06-midterm_review/containers.cpp
+++ lab06-midterm_review/containers.cpp
@@ -3,8 +3,39 @@
 
 using namespace std;
 
+class Longboard
+{
+    double deck_length;
 
-int main(int argc, char ** argv) {
+  public:
+    Longboard(double d_in) : deck_length(d_in) {}
+    double get_deck_length() const
+    {
+        return deck_length;
+    }
+
+    double operator+(const Longboard &l_in1);
+};
+
+double Longboard::operator+(const Longboard &l_in1)
+{
+    return deck_length + l_in1.deck_length;
+}
+// double operator+(const Longboard &l_in1,
+//                  const Longboard &l_in2)
+// {
+//     return l_in1.get_deck_length() +
+//            l_in2.get_deck_length();
+// }
+
+int main(int argc, char **argv)
+{
+    Longboard l1(8);
+    Longboard l2(10);
+    l1.get_deck_length();
+    double d1 = l1 + l2;
+    cout << d1 << endl;
+    cout << l1 + l2 << endl;
 
     return 0;
 }
