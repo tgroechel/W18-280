e31f0d537071e2a715f3a33e5c3cb5f198896d01
tgroeche <tgroeche@umich.edu>
2018-03-16 18:46:44 -0400

    Merge issues

---
+++ merge_issues/hello.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int add(int a, int b){
+	cout << a << endl;
+	cout << b << endl;
+	return a + b;
+}
+
+int sub(int a, int b){
+	cout << a << endl;
+	cout << b << endl;
+	return a - b;
+}
+
+
+int main(){
+	cout << add(2,2) << endl;
+	cout << sub(2,2) << endl;
+	cout << "This is done  " << endl;
+}
