49cefb122d013fd436f88360d44753c6e1856d02
tgroeche <tgroeche@umich.edu>
2018-03-30 12:49:44 -0400

    week9 sdfjk

--- lab09-functors/functors.cpp
+++ lab09-functors/functors.cpp
@@ -5,7 +5,38 @@
 
 using namespace std;
 
+class A {
+    string trojans;
+
+  public:
+    A(string s_in) : trojans(s_in) {}
+    bool operator()(string something) const { return trojans == something; }
+};
+
+class Bee {
+    int trojans;
+
+  public:
+    Bee(int s_in) : trojans(s_in) {}
+    bool operator()(int something) const { return trojans < something; }
+};
+
+template <typename Potato, typename Tomato>
+void print_on_same(Potato p1, Tomato t1) {
+    cout << p1(t1) << endl;
+}
+
 int main() {
-    cout << "Hello" << endl;
+    A aye("pigs");
+    A aye2("doglets");
+
+    cout << aye("doglets") << endl;  // 0
+    cout << aye2("doglets") << endl; // 1
+    cout << aye("pigs") << endl;     // 1
+
+    Bee b1(7);
+    print_on_same(aye, "pigs"); // 1
+    print_on_same(b1, 9);       // 1
+
     return 0;
 }
