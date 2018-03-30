99545ab22a8f81006545aafb212247f841b9a70f
tgroeche <tgroeche@umich.edu>
2018-03-30 15:08:19 -0400

    Functors done, getting to other pushes

--- lab09-functors/functors.cpp
+++ lab09-functors/functors.cpp
@@ -2,13 +2,14 @@
 #include <map>
 #include <string>
 #include <vector>
+#include <set>
 
 using namespace std;
 
 class A {
     string trojans;
 
-  public:
+public:
     A(string s_in) : trojans(s_in) {}
     bool operator()(string something) const { return trojans == something; }
 };
@@ -16,9 +17,12 @@ class A {
 class Bee {
     int trojans;
 
-  public:
+public:
     Bee(int s_in) : trojans(s_in) {}
-    bool operator()(int something) const { return trojans < something; }
+    bool operator()(int something) const {
+        something = something + 9 / 10;
+        return trojans < something;
+    }
 };
 
 template <typename Potato, typename Tomato>
@@ -26,17 +30,52 @@ void print_on_same(Potato p1, Tomato t1) {
     cout << p1(t1) << endl;
 }
 
+void get_phd(int year) {
+    if (year >= 4) {
+        return;
+    }
+    cout << year << endl;
+    get_phd(year + 1);
+    cout << year << endl;
+
+}
+
+
 int main() {
-    A aye("pigs");
-    A aye2("doglets");
 
-    cout << aye("doglets") << endl;  // 0
-    cout << aye2("doglets") << endl; // 1
-    cout << aye("pigs") << endl;     // 1
+    get_phd(0);
 
-    Bee b1(7);
-    print_on_same(aye, "pigs"); // 1
-    print_on_same(b1, 9);       // 1
+    vector<string> words = {"a", "tom", "a", "kldsf", "potato", "alphabet", "alphabet"};
+    map<string, int> m1;
+    set<string> s1;
+
+    for (auto it : words) {
+        s1.insert(it);
+    }
+    for (auto it : s1) {
+        cout << it << endl;
+    }
+
+    for (string it : words) {
+        if (m1.find(it) == m1.end()) {
+            m1[it] = 1;
+        }
+        else {
+            m1[it] = m1[it] + 1;
+        }
+    }
+    // map<string, map<double,int> > m2;
+
+    //m2["tom"][7.7] : 5
+
+    //cout << m1["a"] << endl;
+    pair<int, double> p1 = {2, 8.8};
+    cout << p1.second << endl;
+    for (pair<string, int> it : m1) {
+        cout << it.first << ": " << it.second << endl;
+    }
+
+
 
     return 0;
 }
