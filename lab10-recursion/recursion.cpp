a2b9a5a41f87f24b3dac8806e5f1fe45a749e09e
tgroeche <tgroeche@umich.edu>
2018-04-06 11:01:35 -0400

    Open

--- lab10-recursion/recursion.cpp
+++ lab10-recursion/recursion.cpp
@@ -1,33 +1,11 @@
 #include <iostream>
-#include <vector>
-#include <string>
 #include <map>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-
-int print_stuff(int num){
-	cout << num << endl;
-	if(num <= 0){
-		return 1;
-	}
-	int hold = print_stuff(num - 1);
-	cout << hold << endl;
-	return hold + 1;
-}
-
-struct fs_direntry {
-    char name[59 + 1];         // name of this file or directory
-    uint32_t inode_block;                  // disk block that stores the inode for
-                                           // this file or directory
-};
-
 int main() {
-	for(int i = 0; i < 59; ++i){
-		cout << 'a';
-	}
-	cout << endl;
-
-	cout << (512/sizeof(fs_direntry)) << endl;
-
+    cout << "recursion" << endl;
+    return 0;
 }
